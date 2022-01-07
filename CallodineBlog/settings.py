aa55303d308c442829739d5cef3402e329ed2910
Sri Balaji Muruganandam <dmsribalaji@zoho.com>
2022-01-07 09:43:57 -0500

    Add Password reset for the users

--- CallodineBlog/settings.py
+++ CallodineBlog/settings.py
@@ -167,3 +167,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+EMAIL_HOST_USER = "algolegion@gmail.com"
+EMAIL_HOST_PASSWORD = "algolegion2020"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
